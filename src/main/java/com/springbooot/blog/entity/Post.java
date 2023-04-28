76d0fd7e9b4744044e90049f4506f19ca0601ca3
Isuru Wimalasiri <isuru.wimalasiri1997@gmail.com>
2023-04-28 16:24:12 +0530

    Use ModelMapper to make object mapping easy.

--- src/main/java/com/springbooot/blog/entity/Post.java
+++ src/main/java/com/springbooot/blog/entity/Post.java
@@ -1,14 +1,13 @@
 package com.springbooot.blog.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -26,5 +25,5 @@ public class Post {
     private String content;
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Comment> comment = new HashSet<>();
+    private Set<Comment> comments = new HashSet<>();
 }
